d291c7a2e7be228277129331766ad5d1629e11f9
lucasb-silva <lucasbeserrasilva18@gmail.com>
2023-07-25 21:51:28 -0300

    Modelando o cadastro de filmes

---
+++ src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -0,0 +1,35 @@
+package br.com.alura.screenmatch.controller;
+
+import br.com.alura.screenmatch.domain.filme.DadosCadastroFilme;
+import br.com.alura.screenmatch.domain.filme.Filme;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+// Mapeando a requisição
+@RequestMapping("/filmes")
+public class FilmeController {
+
+    private List<Filme> filmes = new ArrayList<>();
+
+    // Retorno de uma requisição GET
+    @GetMapping
+    public String carregaPaginaFormulario(){
+        return "filmes/formulario";
+    }
+
+    @PostMapping
+    public String cadastraFilme(DadosCadastroFilme dados) {
+        var filme = new Filme(dados);
+        filmes.add(filme);
+
+        System.out.println(filmes);
+
+        return "filmes/formulario";
+    }
+}
