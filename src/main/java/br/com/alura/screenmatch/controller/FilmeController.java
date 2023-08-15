8bf9fe4147c308f3853e47ff0729149aefe05ffd
lucasb-silva <lucasbeserrasilva18@gmail.com>
2023-08-15 18:09:54 -0300

    Interagindo com a parte visual da app

--- src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -3,6 +3,7 @@ package br.com.alura.screenmatch.controller;
 import br.com.alura.screenmatch.domain.filme.DadosCadastroFilme;
 import br.com.alura.screenmatch.domain.filme.Filme;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,15 @@ public class FilmeController {
     private List<Filme> filmes = new ArrayList<>();
 
     // Retorno de uma requisição GET
-    @GetMapping
+    @GetMapping("/formulario")
     public String carregaPaginaFormulario(){
         return "filmes/formulario";
+    }
+
+    @GetMapping
+    public String carregaPaginaListagem(Model model){
+        model.addAttribute("lista",filmes);
+        return "filmes/listagem";
     }
 
     @PostMapping
@@ -28,8 +35,6 @@ public class FilmeController {
         var filme = new Filme(dados);
         filmes.add(filme);
 
-        System.out.println(filmes);
-
-        return "filmes/formulario";
+        return "redirect:/filmes";
     }
 }
